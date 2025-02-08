7cc119c284ed2c9604c8e17832d54ef34efc2951
propolisss <artem89296145449@gmail.com>
2025-02-09 01:15:52 +0300

    done

---
+++ source/compressObject.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Функция, которая принимает объект и возвращает новый объект, содержащий
+ * только те ключи, которые имеют значения, отличные от null, undefined или
+ * пустой строки.
+ * @param {Object} sourceObj - исходный объект
+ * 
+ * @example
+ * // returns {a: 42} 
+ * compressObject({a: 42, b: null, c: "", d: undefined});
+ * 
+ * @returns {Object}
+ */
+const compressObject = (sourceObj) =>
+    Object
+        .entries(sourceObj)
+        .reduce((compressedObj, [key, value]) => {
+            if (value !== '' && value !== null && value !== undefined) {
+                compressedObj[key] = structuredClone(value);
+            }
+            return compressedObj;
+        }, {});
--- test/compressObject.js
+++ test/compressObject.js
@@ -28,4 +28,39 @@ QUnit.module("Тестируем функцию compressObject", function() {
 
         assert.deepEqual(result, {}, "Пустой объект должен вернуть пустой объект.");
     });
+
+    QUnit.test("Работает с массивом", function(assert) {
+        const result = compressObject({
+            array: [1, 2, 3],
+        });
+
+        assert.deepEqual(result, { array: [1, 2, 3] }, "Массив должен остаться.");
+    });
+
+    QUnit.test("Работает с вложенным объектом", function(assert) {
+        const result = compressObject({
+            innerObject: {
+                a: 1,
+                d: null,
+                e: undefined,
+                f: '',
+            },
+        });
+
+        assert.deepEqual(result, {
+            innerObject: { a: 1, d: null, e: undefined, f: '' },
+        }, "Объект должен остаться и содержать все поля.");
+    });
+
+    QUnit.test(`Работает с нестандартными значениями`, function(assert) {
+        const result = compressObject({
+            a: '0',
+            b: 0,
+            c: 100n,
+            d: false,
+            e: NaN,
+        });
+
+        assert.deepEqual(result, { a: '0', b: 0, c: 100n, d: false, e: NaN }, "Все значения должны остаться.");
+    });
 });
