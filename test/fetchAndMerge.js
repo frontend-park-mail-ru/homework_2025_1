a2de1019c03c9de65f292acbd10d7a6865051ee2
Александр Гароев <32778329+derletzte255@users.noreply.github.com>
2025-02-09 15:54:00 +0300

    Added more tests for fetchAndMergeData

--- test/fetchAndMerge.js
+++ test/fetchAndMerge.js
@@ -43,5 +43,41 @@ QUnit.module("Тестируем функцию fetchAndMerge", function() {
         const result = await fetchAndMergeData(urls);
         assert.deepEqual(result, {}, "Должно возвращать пустой объект при ошибке fetch");
     });
+
+    // Авторские тесты)
+
+    QUnit.test("Работает правильно при пустом массиве URL", async function(assert) {
+        const urls = [];
+        const result = await fetchAndMergeData(urls);
+        assert.deepEqual(result, {}, "Должно возвращать пустой объект при пустом массиве URL");
+    });
+
+    QUnit.test("Работает правильно при дублирующихся значениях", async function(assert) {
+        const urls = [
+            'https://vk.example.com/vkid',
+            'https://vk.example.com/vkid',
+        ];
+        const expected = {
+            "age": [25],
+            "id": [1],
+            "name": ["Олег"],
+            "surname": ["Петров"],
+            "status": ["Дуров, верни стену!"],
+        };
+        
+        window.fetch = (url) => {
+            const data = {
+                'https://vk.example.com/vkid': { "id": 1, "name": "Олег", "surname": "Петров", "age": 25, "status": "Дуров, верни стену!" },
+            };
+
+            return Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve(data[url]),
+            });
+        };
+
+        const result = await fetchAndMergeData(urls);
+        assert.deepEqual(result, expected, "Должно добавлять уникальные значения по полю");
+    });
 });
 
