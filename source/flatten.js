0fa0395f6777b07db1602361a9ae8303f51eee49
Xeonoff <72578165+Xeonoff@users.noreply.github.com>
2025-02-09 19:17:20 +0300

    WEB-22 Малютин Илья Вариант 12

---
+++ source/flatten.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Array} arr
+ * @example
+ * //returns [1, 2, 3]
+ * flatten([1, 2, 3]);
+ * @returns {Array} 
+ */
+function flatten(arr) {
+    if (!Array.isArray(arr)) {
+        throw new TypeError("Аргумент должен быть массивом");
+    }
+
+    return arr.reduce((acc, item) => {
+        if (Array.isArray(item)) {
+            return acc.concat(flatten(item));
+        } else {
+            return acc.concat(item); 
+        }
+    }, []);
+}
--- test/flatten.js
+++ test/flatten.js
@@ -3,17 +3,30 @@
 QUnit.module("Тестируем функцию flatten", function() {
     QUnit.test("Работает правильно с плоским массивом", function(assert) {
         const result = flatten([1, 2, 3]);
-
-        assert.deepEqual(result, [1, 2, 3]);
+        assert.deepEqual(result, [1, 2, 3], "flatten([1, 2, 3]) == [1, 2, 3]");
     });
 
     QUnit.test("Работает правильно с вложенным массивом с несколькими уровнями", function(assert) {
         const result = flatten([1, [2, [3, 4], 5], 6]);
-        assert.deepEqual(result, [1, 2, 3, 4, 5, 6]);
+        assert.deepEqual(result, [1, 2, 3, 4, 5, 6], "flatten([1, [2, [3, 4], 5], 6]) == [1, 2, 3, 4, 5, 6]");
     });
 
     QUnit.test("Работает правильно с пустым массивом", function(assert) {
         const result = flatten([]);
-        assert.deepEqual(result, []);
+        assert.deepEqual(result, [], "flatten([]) == []");
+    });
+
+    QUnit.test("Работает правильно с массивами, содержащими разные типы данных", function(assert) {
+        const result = flatten([1, [2, ["three", true], null], { key: "value" }]);
+        assert.deepEqual(result, [1, 2, "three", true, null, { key: "value" }], "flatten([1, [2, ['three', true], null], { key: 'value' }]) == [1, 2, 'three', true, null, { key: 'value' }]");
+    });
+
+    QUnit.test("Работает правильно с глубоко вложенными массивами", function(assert) {
+        const result = flatten([1, [2, [3, [4, [5]]]]]);
+        assert.deepEqual(result, [1, 2, 3, 4, 5], "flatten([1, [2, [3, [4, [5]]]]]) == [1, 2, 3, 4, 5]");
+    });
+
+    QUnit.test("Выбрасывает ошибку, если аргумент не является массивом", function(assert) {
+        assert.throws(() => flatten("not an array"), TypeError, "Аргумент должен быть массивом");
     });
 });
