3e6389cd14be754b720ee225eb346a54a668a596
vcreatorv <valery.nagapetyan@yandex.ru>
2025-02-09 13:46:24 +0300

    Реализовал функцию transform и добавил тесты для проверки работы с пустыми объектами, null и undefined, а также с объектами с глубокой вложенностью.

---
+++ source/transform.js
@@ -0,0 +1,22 @@
+/**
+ * Функция для рекурсивного преобразования значений в объекте
+ * @param {Object} obj - Исходный объект для преобразования
+ * @param {Function} transformFn - Функция преобразования, применяемая к значениям
+ * 
+ * @example
+ * // returns { a: 2, b: 4, c: 6 }
+ * transform({ a: 1, b: 2, c: 3 }, value => value * 2);
+ *
+ * @returns {Object} - Новый объект с преобразованными значениями
+ */
+const transform = (obj, transformFn) => {
+    if (Array.isArray(obj)) {
+        return obj.map(item => transform(item, transformFn));
+    }
+    if (obj !== null && typeof obj === 'object') {
+        return Object.fromEntries(
+            Object.entries(obj).map(([key, value]) => [key, transform(value, transformFn)])
+        );
+    }
+    return transformFn(obj);
+};
--- test/transform.js
+++ test/transform.js
@@ -24,4 +24,40 @@ QUnit.module('Тестируем функцию transform', () => {
 
         assert.deepEqual(result, { a: [3, 6, 9], b: 12 }, 'Элементы массива должны быть умножены на 3');
     });
+
+    QUnit.test('Работает правильно с пустым объектом', (assert) => {
+        const result = transform({}, (val) => val * 2);
+        assert.deepEqual(result, {}, 'Пустой объект остается пустым');
+    });
+    
+    QUnit.test('Работает правильно с null и undefined', (assert) => {
+        const result = transform({ a: null, b: undefined }, (val) => val ?? 'default');
+        assert.deepEqual(result, { a: 'default', b: 'default' }, 'null и undefined заменяются на "default"');
+    });
+
+    QUnit.test('Работает правильно с глубоко вложенными объектами', (assert) => {
+        const originalObject = { 
+            a: 1, 
+            b: { 
+                c: 2, 
+                d: { 
+                    e: 3, 
+                    f: { g: 4 } 
+                } 
+            } 
+        };
+        const transformFunction = (value) => value * 10;
+        const result = transform(originalObject, transformFunction);
+    
+        assert.deepEqual(result, { 
+            a: 10, 
+            b: { 
+                c: 20, 
+                d: { 
+                    e: 30, 
+                    f: { g: 40 } 
+                } 
+            } 
+        }, 'Все значения в глубоко вложенных объектах должны быть умножены на 10');
+    });
 });
