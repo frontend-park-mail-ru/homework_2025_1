193177512a103c61c7091e527efc96c5ca2fe665
Vadim Pichurin <103596240+Vadimpich@users.noreply.github.com>
2025-02-10 12:11:50 +0300

    Обновлена документация, добавлена Arrow function

--- source/flatten.js
+++ source/flatten.js
@@ -2,16 +2,20 @@
 
 /**
  * Функция, которая приводит вложенные массивы к одному массиву
- * @param {Array<number | Array>} arr - массив чисел, возможно вложенный
+ * @param {Array<any | Array>} arr - массив элементов любого типа, возможно вложенный
  *
  * @example
  * // returns [1, 2, 3, 4, 5]
  * flatten([1, [2, 3, [4, 5]]]);
  *
- * @returns {Array<number>} - массив чисел без вложенности
+ * @example
+ * // returns [1, 'a', true, null, { key: 'value' }]
+ * flatten([1, ['a', [true, null]], { key: 'value' }]);
+ *
+ * @returns {Array<any>} - массив элементов без вложенности
  * @throws {TypeError} - если передан не массив
  */
-function flatten(arr) {
+const flatten = (arr) => {
     if (!Array.isArray(arr)) {
         throw new TypeError("Ожидался массив");
     }
@@ -24,4 +28,4 @@ function flatten(arr) {
         }
         return acc;
     }, []);
-}
+};
