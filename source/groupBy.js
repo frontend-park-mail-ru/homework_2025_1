9bcfcedf147bfed707ec1908780de378d4ed539c
MarrriaEM <mme.06072003@gmail.com>
2025-02-11 00:27:12 +0300

    Внесены правки
    проверка входных параметров + тесты,  замена цикла for на метод reduce,  название array на objectsToGroup,

--- source/groupBy.js
+++ source/groupBy.js
@@ -2,8 +2,8 @@
 
 /**
  * Группирует массив объектов по указанному ключу.
- * @param {Array<Object>} array - массив объектов для группировки
- * @param {string} key - ключ, по которому производится группировка
+ * @param {Array<Object>} objectsToGroup - массив объектов для группировки
+ * @param {string|number} key - ключ, по которому производится группировка (строка или число)
  * 
  * @example
  * // returns { fruit: [{ id: 1, category: 'fruit', name: 'apple' }], vegetable: [{ id: 3, category: 'vegetable', name: 'carrot' }] }
@@ -11,18 +11,24 @@
  * 
  * @returns {Object} - объект, где ключи — уникальные значения по указанному ключу, а значения — массивы объектов, соответствующих этим ключам
  */
-function groupBy(array, key) {
-    const result = {}; 
-
-    for (const element of array) { 
-        const groupKey = element[key]; 
-
-        if (!result[groupKey]) { 
-            result[groupKey] = []; 
-        }
-
-        result[groupKey].push(element); 
+const groupBy = (objectsToGroup, key) => {
+    if (!Array.isArray(objectsToGroup)) {
+        throw new TypeError('Первый аргумент должен быть массивом');
     }
 
-    return result; 
-} 
+    if (typeof key !== 'string' && typeof key !== 'number') {
+        throw new TypeError('Ключ должен быть строкой или числом');
+    }
+
+    return objectsToGroup.reduce((result, element) => {
+        const groupKey = element[key];
+
+        if (!result[groupKey]) {
+            result[groupKey] = [];
+        }
+
+        result[groupKey].push(element);
+
+        return result;
+    }, {});
+};
