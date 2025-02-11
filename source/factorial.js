35758bc2b9e985c2ba36f1bcb4993bfaed2ab3ba
zeritonik <zaq31337@gmail.com>
2025-02-11 12:55:32 +0300

    Added n is number check, added empty line in the end of factorial.js, added tests for exceptions

--- source/factorial.js
+++ source/factorial.js
@@ -15,6 +15,9 @@
  * @returns {Number}
  */
 function factorial(n) {
+    if (typeof(n) !== 'number') {
+        throw new Error('Факториал определен только для чисел');
+    }
     if (n < 0) {
         throw new Error('Факториал не определен для отрицательных чисел');
     }
