73490d6e05846d9ddb9ecf71a7deef314250ef50
zeritonik <zaq31337@gmail.com>
2025-02-11 13:51:57 +0300

    removed brackets from typeof

--- source/factorial.js
+++ source/factorial.js
@@ -15,12 +15,13 @@
  * @returns {Number}
  */
 function factorial(n) {
-    if (typeof(n) !== 'number') {
+    if (typeof n !== 'number') {
         throw new Error('Факториал определен только для чисел');
     }
     if (n < 0) {
         throw new Error('Факториал не определен для отрицательных чисел');
     }
+    
     let res = 1;
     for (let i = 1; i <= n; i++) {
         res *= i;
