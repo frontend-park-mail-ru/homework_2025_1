757b0bfc0bb670fb385469950a6a27e2007aae39
Valekir <alexkir15@yandex.ru>
2025-02-12 17:43:54 +0300

    Доработата функция groupBy, написаны дополнительные тесты

--- source/groupBy.js
+++ source/groupBy.js
@@ -3,19 +3,28 @@
 /**
  * Функция, группирующая объекты из массива по типу
  * @param {Array<Objects>} input - массив объектов
+ * @param key - ключ, по которому группируются элементы массива 
  * 
  * @returns {type: {Objects}}
  */
-const groupBy = (input) => {
-    let result = {};
-    for (let i of input) {
-        let category = i["category"];
-
-        if (category in result) {
-            result[category].push(i);
-        } else {
-            result[category] = [i];
-        }
+const groupBy = (input, key) => {
+    if (typeof(key) != 'string') {
+        key = 'category';
     }
-     return result;
+    if (!Array.isArray(input)) {
+        throw new TypeError('Первый аргумент должен быть массивом объектов');
+    }
+    const itemsByCategory = input.reduce(function(result, object) {
+        if (!result[object[key]]) {
+            result[object[key]] = [object];
+        } else {
+            result[object[key]].push(object);
+        }
+        return result;
+    }, {})
+    return itemsByCategory;
 }
+
+// function groupBy(input) {
+//     return Object.groupBy(input, ({category}) => category);
+// }
--- test/groupBy.js
+++ test/groupBy.js
@@ -50,41 +50,152 @@ QUnit.module('Тестируем функцию groupBy', () => {
 
     QUnit.test('Работает правильно с не-объектами', assert => {
         const input = [
-            { id: 1, category: "fruit", name: "apple" },
-            "invalid", 
-            { id: 2, category: "vegetable", name: "carrot" }
+            { id: 1, category: 'fruit', name: 'apple' },
+            'invalid', 
+            { id: 2, category: 'vegetable', name: 'carrot' }
         ];
-        const expected = {
+        const result = groupBy(input, 'category');
+
+        assert.deepEqual(result, {
             fruit: [
-              { id: 1, category: "fruit", name: "apple" }
+              { id: 1, category: 'fruit', name: 'apple' }
             ],
-            undefined: ["invalid"],
+            undefined: ['invalid'],
             vegetable: [
-              { id: 2, category: "vegetable", name: "carrot" }
+              { id: 2, category: 'vegetable', name: 'carrot' }
             ]
-        };
-        const result = groupBy(input);
-        assert.deepEqual(result, expected, 'Не-объекты были записаны в категорию "undefined"');
+        }, 'Не-объекты были записаны в категорию "undefined"');
     });
 
-QUnit.test('Работает правильно с объектами без категории', assert => {
+    QUnit.test('Работает правильно с объектами без категории', assert => {
         const input = [
-            { id: 1, category: "fruit", name: "apple" },
-            { id: 2, name: "unknown" }, 
-            { id: 3, category: "vegetable", name: "carrot" }
+            { id: 1, category: 'fruit', name: 'apple' },
+            { id: 2, name: 'unknown' }, 
+            { id: 3, category: 'vegetable', name: 'carrot' }
+        ];
+        const result = groupBy(input, 'category');
+
+        assert.deepEqual(result, {
+            fruit: [
+                { id: 1, category: 'fruit', name: 'apple' }
+            ],
+            undefined: [
+              { id: 2, name: 'unknown' }
+            ],
+            vegetable: [
+              { id: 3, category: 'vegetable', name: 'carrot' }
+            ]
+          }, 'Объекты без категории были записаны в категорию undefined');
+    });
+
+    QUnit.test('Работает правильно с числовыми ключами', assert => {
+        const input = [
+            { 14: 1, category: 'fruit', name: 'apple' },
+            { 100000000000: 2, category:'fruit', name: 'orange' }, 
+            { 0: 3, category: 'vegetable', name: 'carrot' }
+        ];
+        const result = groupBy(input, 'category');
+
+        const expected = {
+            fruit: [
+                { '14': 1, category: 'fruit', name: 'apple' },
+                { '100000000000': 2, category:'fruit', name: 'orange' }
+            ],
+            vegetable: [
+              { '0': 3, category: 'vegetable', name: 'carrot' }
+            ]
+          };
+        assert.deepEqual(result, expected, 'Числа корректно преобразуются в строки');
+    });
+
+    QUnit.test('Работает правильно с булевыми значениями в ключах', assert => {
+        const input = [
+            { true: 1, category: 'fruit', name: 'apple' },
+            { false: 2, category:'fruit', name: 'orange' }, 
+            { true: 3, category: 'vegetable', name: 'carrot' }
         ];
         const expected = {
             fruit: [
-                { id: 1, category: "fruit", name: "apple" }
-            ],
-            undefined: [
-              { id: 2, name: "unknown" }
+                { 'true': 1, category: 'fruit', name: 'apple' },
+                { 'false': 2, category:'fruit', name: 'orange' }, 
             ],
             vegetable: [
-              { id: 3, category: "vegetable", name: "carrot" }
+              { 'true': 3, category: 'vegetable', name: 'carrot' }
             ]
           };
-        const result = groupBy(input);
-        assert.deepEqual(result, expected, 'Объекты без категории были записаны в категорию undefined');
+        const result = groupBy(input, 'category');
+        assert.deepEqual(result, expected, 'Числа корректно преобразуются в строки');
+    });
+
+    QUnit.test('Работает правильно с объект строками в значении атрибутов', (assert) => {
+        const data = [
+            { id: 1, category: new String('fruit'), name: new String('apple') },
+            { id: 2, category: new String('fruit'), name: new String('banana') },
+            { id: 3, category: new String('vegetable'), name: new String('carrot') },
+            { id: 4, category: new String('fruit'), name: new String('orange') },
+            { id: 5, category: new String('vegetable'), name: new String('lettuce') }
+        ];
+        const result = groupBy(data, 'category');
+    
+        assert.deepEqual(result, {
+            fruit: [
+                { id: 1, category: 'fruit', name: 'apple' },
+                { id: 2, category: 'fruit', name: 'banana' },
+                { id: 4, category: 'fruit', name: 'orange' }
+            ],
+            vegetable: [
+                { id: 3, category: 'vegetable', name: 'carrot' },
+                { id: 5, category: 'vegetable', name: 'lettuce' }
+            ]
+        }, 'Объекты должны быть сгруппированы по категории');
+    });
+
+    
+    QUnit.test('Работает правильно если передать первым аргументом не массив', (assert) => {
+        const data = 123;
+        assert.throws(() => {
+            groupBy(data, 123)
+        }, TypeError, 'Ошибка выбрасывается, если первый аргумент функции не массив объектов');
+    });
+
+    QUnit.test('Работает правильно если ключ для группировки аргументов не строка', (assert) => {
+        const data1 = [
+            { id: 1, category: 'fruit', name: 'apple' },
+            { id: 2, category: 'fruit', name: 'banana' },
+            { id: 3, category: 'vegetable', name: 'carrot' },
+            { id: 4, category: 'fruit', name: 'orange' },
+            { id: 5, category: 'vegetable', name: 'lettuce' }
+        ];
+        const data2 = [
+            { id: 1,  name: 'apple' },
+            { id: 2,  name: 'banana' },
+            { id: 3,  name: 'carrot' },
+            { id: 4,  name: 'orange' },
+            { id: 5,  name: 'lettuce' }
+        ];
+        const result1 = groupBy(data1, 123);
+        const result2 = groupBy(data2, 123);
+    
+        assert.deepEqual(result1, {
+            fruit: [
+                { id: 1, category: 'fruit', name: 'apple' },
+                { id: 2, category: 'fruit', name: 'banana' },
+                { id: 4, category: 'fruit', name: 'orange' }
+            ],
+            vegetable: [
+                { id: 3, category: 'vegetable', name: 'carrot' },
+                { id: 5, category: 'vegetable', name: 'lettuce' }
+            ],
+        }, 'Если у объектов есть категория, то они должны быть сгруппированы по категориям');
+        assert.deepEqual(result2, {
+            undefined: [
+                { id: 1,  name: 'apple' },
+                { id: 2,  name: 'banana' },
+                { id: 3,  name: 'carrot' },
+                { id: 4,  name: 'orange' },
+                { id: 5,  name: 'lettuce' }
+            ],
+        }, 'Если у объектов не указана категория, то они должны быть сгруппированы в единый массив');
+        
     });
 });
