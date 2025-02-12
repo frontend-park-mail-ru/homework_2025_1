50b72c5cf1deb735cd365d4a435f82c2ecc7ec2c
ForgeUp <sh.383.fank@mail.ru>
2025-02-12 18:37:56 +0300

    Improve compressObject

--- source/compressObject.js
+++ source/compressObject.js
@@ -23,13 +23,11 @@ const compressObject = (obj) => {
         throw new TypeError(`Expected type '[object Object]', but received '${Object.prototype.toString.call(obj)}'`);
     }
     
-    let result = Object.entries(obj).reduce((acc, [key, value]) => {
+    return Object.entries(obj).reduce((acc, [key, value]) => {
         if (value !== undefined && value !== null && value !== "") {
             acc[key] = value;
         }
         return acc;
     }, {});
-
-    return result;
 };
 
